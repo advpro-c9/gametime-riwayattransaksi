251136d27ea9b6ea5d4ed449a4fc50f82a7cea2b
Farrel Sheva <farrelshevaa@gmail.com>
2024-04-17 18:21:40 +0700

    Init commit

---
+++ src/main/java/com/c9/riwayatTransaksi/GametimeRiwayatTransaksiApplication.java
@@ -0,0 +1,13 @@
+package com.c9.riwayatTransaksi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametimeRiwayatTransaksiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametimeRiwayatTransaksiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c9/riwayatTransaksi/GametimeRiwayatTransaksiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c9.riwayatTransaksi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametimeRiwayatTransaksiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
